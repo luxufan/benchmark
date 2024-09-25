deb501aef3b3baa5f7bfb0faeaf04551ab15559a
luxufan <luxufan981014@gmail.com>
2024-09-25 17:36:07 +0000

    fix build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "$PWD"
 
 # Build llvm project
 #cd toolchain/llvm-project
-#git checkout 62f6b9a93e2773c4e369ec1673155fad4adab02e
+#git checkout envoy-thinlto
 #rm -rf build-release
 #mkdir build-release
 #cd build-release
@@ -17,33 +17,33 @@ echo "$PWD"
 #
 #cd $BASEDIR
 #
-#export PATH="$PWDDIR/toolchain/llvm-project/build-release/bin:$PATH"
-#echo "$(which clang)"
-
-
-rm -rf ~/bazel-bin
-mkdir ~/bazel-bin
-wget -O ~/bazel-bin/bazel https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-$([ $(uname -m) = "aarch64" ] && echo "arm64" || echo "amd64")
-chmod +x ~/bazel-bin/bazel
+export PATH="$PWDDIR/toolchain/llvm-project/build-release/bin:$PATH"
+echo "$(which clang)"
+#
+#
+#rm -rf ~/bazel-bin
+#mkdir ~/bazel-bin
+#wget -O ~/bazel-bin/bazel https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-$([ $(uname -m) = "aarch64" ] && echo "arm64" || echo "amd64")
+#chmod +x ~/bazel-bin/bazel
 export PATH="~/bazel-bin:$PATH"
-
-mkdir out
-rm -rf out/envoy
-mkdir out/envoy
-rm -rf out/envoy/thinlto-dyncastopt
-mkdir out/envoy/thinlto-dyncastopt
-
-cd test-suites/envoy
-bazel clean
-
-bazel build --config=libc++ --copt="-Wno-error=thread-safety-reference-return" --linkopt="-fuse-ld=lld" --linkopt="-save-stats=obj" --linkopt="-flto=thin" --linkopt="-Wl,--lto-whole-program-visibility" --copt="-Wno-error=unused-command-line-argument" --copt="-fwhole-program-vtables" --copt="-fuse-ld=lld" --copt="-flto=thin" --copt="-O2" envoy
-
-# To get the statisitcs file
-clang++ @bazel-out/k8-fastbuild/bin/source/exe/envoy-static-2.params
-
-mv bazel-out/k8-fastbuild/bin/source/exe/envoy-static $PWDDIR/out/envoy/thinlto-dyncastopt/
-mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/envoy/thinlto-dyncastopt/envoy-static.stats
-
+#
+#mkdir out
+#rm -rf out/envoy
+#mkdir out/envoy
+#rm -rf out/envoy/thinlto-dyncastopt
+#mkdir out/envoy/thinlto-dyncastopt
+#
+#cd $PWDDIR/test-suites/envoy
+##bazel clean
+#
+#bazel build --config=libc++ --copt="-Wno-error=thread-safety-reference-return" --linkopt="-fuse-ld=lld" --linkopt="-save-stats=obj" --linkopt="-flto=thin" --linkopt="-Wl,--lto-whole-program-visibility" --copt="-Wno-error=unused-command-line-argument" --copt="-fwhole-program-vtables" --copt="-fuse-ld=lld" --copt="-flto=thin" --copt="-O2" envoy
+#
+## To get the statisitcs file
+#clang++ @bazel-out/k8-fastbuild/bin/source/exe/envoy-static-2.params
+#
+#mv bazel-out/k8-fastbuild/bin/source/exe/envoy-static $PWDDIR/out/envoy/thinlto-dyncastopt/
+#mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/envoy/thinlto-dyncastopt/envoy-static.stats
+#
 #rm -rf $PWDDIR/out/envoy/thinlto
 #mkdir $PWDDIR/out/envoy/thinlto
 #
@@ -54,8 +54,8 @@ mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/env
 #
 #mv bazel-out/k8-fastbuild/bin/source/exe/envoy-static $PWDDIR/out/envoy/thinlto/
 #mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/envoy/thinlto/envoy-static.stats
-#
-###### Full lto
+##
+####### Full lto
 #rm -rf $PWDDIR/out/envoy/fulllto
 #mkdir $PWDDIR/out/envoy/fulllto
 #
@@ -65,8 +65,22 @@ mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/env
 #clang++ @bazel-out/k8-fastbuild/bin/source/exe/envoy-static-2.params
 #mv bazel-out/k8-fastbuild/bin/source/exe/envoy-static $PWDDIR/out/envoy/fulllto/
 #mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/envoy/fulllto/envoy-static.stats
-#
-###### Full lto with dyncastopt
+
+# Build llvm project
+cd toolchain/llvm-project
+git checkout dyncast
+rm -rf build-release
+mkdir build-release
+cd build-release
+
+cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
+
+ninja
+
+cd $BASEDIR
+
+
+##### Full lto with dyncastopt
 #rm -rf $PWDDIR/out/envoy/fulllto-dyncastopt
 #mkdir $PWDDIR/out/envoy/fulllto-dyncastopt
 #
@@ -76,44 +90,43 @@ mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/env
 #clang++ @bazel-out/k8-fastbuild/bin/source/exe/envoy-static-2.params
 #mv bazel-out/k8-fastbuild/bin/source/exe/envoy-static $PWDDIR/out/envoy/fulllto-dyncastopt/
 #mv bazel-out/k8-fastbuild/bin/source/exe/version_linkstamp.stats $PWDDIR/out/envoy/fulllto-dyncastopt/envoy-static.stats
-#
+
 ##### Build blender
-#rm -rf $PWDDIR/out/blender
-#mkdir $PWDDIR/out/blender
-#
-#cd $PWDDIR/test-suites/blender
-#./build_files/utils/make_update.py --use-linux-libraries
-#
-#rm -rf $PWDDIR/test-suites/build_linux
-#CC=$PWDDIR/toolchain/llvm-project/build-release/bin/clang CXX=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ make
-#
-#rm -rf $PWDDIR/out/blender/thinlto
-#mkdir $PWDDIR/out/blender/thinlto
-#cp $PWDDIR/cmake/blender/thinlto/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
-#make
-#mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/thinlto
-#mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/thinlto/blender.stats
-#
-#rm -rf $PWDDIR/out/blender/thinlto-dyncastopt
-#mkdir $PWDDIR/out/blender/thinlto-dyncastopt
-#cp $PWDDIR/cmake/blender/thinlto-dyncastopt/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
-#cd $PWDDIR/test-suites/blender
-#make
-#mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/thinlto-dyncastopt
-#mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/thinlto-dyncastopt/blender.stats
+rm -rf $PWDDIR/out/blender
+mkdir $PWDDIR/out/blender
 
-#rm -rf $PWDDIR/out/blender/fulllto
-#mkdir $PWDDIR/out/blender/fulllto
-#cp $PWDDIR/cmake/blender/fulllto/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
-#make
-#mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/fulllto
-#mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/fulllto/blender.stats
+cd $PWDDIR/test-suites/blender
+./build_files/utils/make_update.py --use-linux-libraries
 
+rm -rf $PWDDIR/test-suites/build_linux
+CC=$PWDDIR/toolchain/llvm-project/build-release/bin/clang CXX=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ make
 
-#rm -rf $PWDDIR/out/blender/fulllto-dyncastopt
-#mkdir $PWDDIR/out/blender/fulllto-dyncastopt
-#cp $PWDDIR/cmake/blender/fulllto-dyncastopt/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
-#cd $PWDDIR/test-suites/blender
-#make
-#mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/fulllto-dyncastopt
-#mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/fulllto-dyncastopt/blender.stats
+rm -rf $PWDDIR/out/blender/thinlto
+mkdir $PWDDIR/out/blender/thinlto
+cp $PWDDIR/cmake/blender/thinlto/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
+make
+mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/thinlto
+mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/thinlto/blender.stats
+
+rm -rf $PWDDIR/out/blender/thinlto-dyncastopt
+mkdir $PWDDIR/out/blender/thinlto-dyncastopt
+cp $PWDDIR/cmake/blender/thinlto-dyncastopt/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
+cd $PWDDIR/test-suites/blender
+make
+mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/thinlto-dyncastopt
+mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/thinlto-dyncastopt/blender.stats
+
+rm -rf $PWDDIR/out/blender/fulllto
+mkdir $PWDDIR/out/blender/fulllto
+cp $PWDDIR/cmake/blender/fulllto/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
+make
+mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/fulllto
+mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/fulllto/blender.stats
+
+rm -rf $PWDDIR/out/blender/fulllto-dyncastopt
+mkdir $PWDDIR/out/blender/fulllto-dyncastopt
+cp $PWDDIR/cmake/blender/fulllto-dyncastopt/CMakeCache.txt $PWDDIR/test-suites/build_linux/CMakeCache.txt
+cd $PWDDIR/test-suites/blender
+make
+mv $PWDDIR/test-suites/build_linux/bin/blender $PWDDIR/out/blender/fulllto-dyncastopt
+mv $PWDDIR/test-suites/build_linux/bin/creator.cc.stats $PWDDIR/out/blender/fulllto-dyncastopt/blender.stats
