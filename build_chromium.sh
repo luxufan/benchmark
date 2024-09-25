a4e06ed1bba07aae07b65ec6f914099bdb41ab7e
luxufan <luxufan981014@gmail.com>
2024-09-25 17:36:37 +0000

    change build files

--- build_chromium.sh
+++ build_chromium.sh
@@ -4,80 +4,80 @@ cd $BASEDIR
 PWDDIR=$(pwd)
 echo "$PWD"
 
-#cd $PWDDIR/toolchain/llvm-project
-#git checkout clang-19-dyncast
-#rm -rf build-release
-#mkdir build-release
-#cd build-release
-#
-#cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
-#
-#ninja
-#
+cd $PWDDIR/toolchain/llvm-project
+git checkout clang-19-dyncast
+rm -rf build-release
+mkdir build-release
+cd build-release
+
+cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
+
+ninja
+
 export PATH="${HOME}/benchmark/test-suites/chromium/depot_tools:$PATH"
 
-#rm -rf $PWDDIR/out/chromium
-#mkdir $PWDDIR/out/chromium
+rm -rf $PWDDIR/out/chromium
+mkdir $PWDDIR/out/chromium
 
-#rm -rf $PWDDIR/out/chromium/fulllto
-#mkdir $PWDDIR/out/chromium/fulllto
-#cd $PWDDIR/test-suites/chromium/chromium/src
-#rm -rf out/fulllto
-#gn gen out/fulllto
-#git reset --hard HEAD
-#git apply $PWDDIR/cmake/chromium/fulllto.patch
-#cp $PWDDIR/cmake/chromium/args.gn out/fulllto/args.gn
-#autoninja -C out/fulllto chrome
-#mv out/default/chrome $PWDDIR/out/chromium/fulllto
-#mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/fulllto/chrome.stats
+rm -rf $PWDDIR/out/chromium/fulllto
+mkdir $PWDDIR/out/chromium/fulllto
+cd $PWDDIR/test-suites/chromium/chromium/src
+rm -rf out/fulllto
+gn gen out/fulllto
+git reset --hard HEAD
+git apply $PWDDIR/cmake/chromium/fulllto.patch
+cp $PWDDIR/cmake/chromium/args.gn out/fulllto/args.gn
+autoninja -C out/fulllto chrome
+mv out/default/chrome $PWDDIR/out/chromium/fulllto
+mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/fulllto/chrome.stats
 
-#rm -rf $PWDDIR/out/chromium/fulllto-dyncastopt
-#mkdir $PWDDIR/out/chromium/fulllto-dyncastopt
-#cd $PWDDIR/test-suites/chromium/chromium/src
-#rm -rf out/fulllto-dyncastopt
-#gn gen out/fulllto-dyncastopt
-#git reset --hard HEAD
-#git apply $PWDDIR/cmake/chromium/fulllto-dyncastopt.patch
-#cp $PWDDIR/cmake/chromium/args.gn out/fulllto-dyncastopt/args.gn
-#autoninja -C out/fulllto-dyncastopt chrome
-#mv out/default/chrome $PWDDIR/out/chromium/fulllto-dyncastopt
-#mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/fulllto-dyncastopt/chrome.stats
-#
-#
-#
-#
-#cd $PWDDIR/toolchain/llvm-project
-#git checkout ecc3d8a29e23e8710e104c37c1a4f82d5c127479
-#rm -rf build-release
-#mkdir build-release
-#cd build-release
-#
-#cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
-#
-#ninja
+rm -rf $PWDDIR/out/chromium/fulllto-dyncastopt
+mkdir $PWDDIR/out/chromium/fulllto-dyncastopt
+cd $PWDDIR/test-suites/chromium/chromium/src
+rm -rf out/fulllto-dyncastopt
+gn gen out/fulllto-dyncastopt
+git reset --hard HEAD
+git apply $PWDDIR/cmake/chromium/fulllto-dyncastopt.patch
+cp $PWDDIR/cmake/chromium/args.gn out/fulllto-dyncastopt/args.gn
+autoninja -C out/fulllto-dyncastopt chrome
+mv out/default/chrome $PWDDIR/out/chromium/fulllto-dyncastopt
+mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/fulllto-dyncastopt/chrome.stats
 
-#rm -rf $PWDDIR/out/chromium/thinlto
-#mkdir $PWDDIR/out/chromium/thinlto
-#cd $PWDDIR/test-suites/chromium/chromium/src
-#rm -rf out/thinlto
-#gn gen out/thinlto
-#git reset --hard HEAD
-#git apply $PWDDIR/cmake/chromium/thinlto.patch
-#cp $PWDDIR/cmake/chromium/args.gn out/thinlto/args.gn
-#autoninja -C out/thinlto chrome
-#mv out/default/chrome $PWDDIR/out/chromium/thinlto
-#mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/thinlto/chrome.stats
-#
-#rm -rf $PWDDIR/out/chromium/thinlto-dyncastopt
-#mkdir $PWDDIR/out/chromium/thinlto-dyncastopt
-#cd $PWDDIR/test-suites/chromium/chromium/src
-#rm -rf out/thinlto-dyncastopt
-#gn gen out/thinlto-dyncastopt
-#git reset --hard HEAD
-#git apply $PWDDIR/cmake/chromium/thinlto-dyncastopt.patch
-#cp $PWDDIR/cmake/chromium/args.gn out/thinlto-dyncastopt/args.gn
-#autoninja -C out/thinlto-dyncastopt chrome
-#mv out/default/chrome $PWDDIR/out/chromium/thinlto-dyncastopt
+
+
+
+cd $PWDDIR/toolchain/llvm-project
+git checkout ecc3d8a29e23e8710e104c37c1a4f82d5c127479
+rm -rf build-release
+mkdir build-release
+cd build-release
+
+cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
+
+ninja
+
+rm -rf $PWDDIR/out/chromium/thinlto
+mkdir $PWDDIR/out/chromium/thinlto
+cd $PWDDIR/test-suites/chromium/chromium/src
+rm -rf out/thinlto
+gn gen out/thinlto
+git reset --hard HEAD
+git apply $PWDDIR/cmake/chromium/thinlto.patch
+cp $PWDDIR/cmake/chromium/args.gn out/thinlto/args.gn
+autoninja -C out/thinlto chrome
+mv out/default/chrome $PWDDIR/out/chromium/thinlto
+mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/thinlto/chrome.stats
+
+rm -rf $PWDDIR/out/chromium/thinlto-dyncastopt
+mkdir $PWDDIR/out/chromium/thinlto-dyncastopt
+cd $PWDDIR/test-suites/chromium/chromium/src
+rm -rf out/thinlto-dyncastopt
+gn gen out/thinlto-dyncastopt
+git reset --hard HEAD
+git apply $PWDDIR/cmake/chromium/thinlto-dyncastopt.patch
+cp $PWDDIR/cmake/chromium/args.gn out/thinlto-dyncastopt/args.gn
+autoninja -C out/thinlto-dyncastopt chrome
+mv out/default/chrome $PWDDIR/out/chromium/thinlto-dyncastopt
 #mv out/default/chrome_exe_main_aura.stats $PWDDIR/out/chromium/thinlto-dyncastopt/chrome.stats
 
 
--- build_solidity.sh
+++ build_solidity.sh
@@ -5,15 +5,15 @@ PWDDIR=$(pwd)
 echo "$PWD"
 
 # Build llvm project
-cd toolchain/llvm-project
-git checkout remotes/origin/dyncast
-rm -rf build-release
-mkdir build-release
-cd build-release
-
-cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
-
-ninja
+#cd toolchain/llvm-project
+#git checkout remotes/origin/dyncast
+#rm -rf build-release
+#mkdir build-release
+#cd build-release
+#
+#cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" -DLLVM_FORCE_ENABLE_STATS=ON -G Ninja
+#
+#ninja
 
 
 rm -rf $PWDDIR/out/solidity
@@ -24,7 +24,7 @@ mkdir $PWDDIR/out/solidity/thinlto-dyncastopt
 rm -rf $PWDDIR/out/solidity/temp
 mkdir $PWDDIR/out/solidity/temp
 cd $PWDDIR/out/solidity/temp
-cmake $PWDDIR/test-suites/solidity -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ -DCMAKE_C_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang -DCMAKE_CXX_FLAGS="-flto=thin -save-stats=obj -fwhole-program-vtables -fuse-ld=lld -Wl,--lto-whole-program-visibility" -DUSE_Z3=OFF
+cmake $PWDDIR/test-suites/solidity -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ -DCMAKE_C_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang -DCMAKE_CXX_FLAGS="-fvisibility=hidden -flto=thin -save-stats=obj -fwhole-program-vtables -fuse-ld=lld -Wl,--lto-whole-program-visibility" -DUSE_Z3=OFF
 ninja solc
 mv $PWDDIR/out/solidity/temp/solc/solc $PWDDIR/out/solidity/thinlto-dyncastopt
 mv $PWDDIR/out/solidity/temp/solc/main.cpp.stats $PWDDIR/out/solidity/thinlto-dyncastopt/solc.stats
@@ -34,7 +34,7 @@ mkdir $PWDDIR/out/solidity/thinlto
 rm -rf $PWDDIR/out/solidity/temp
 mkdir $PWDDIR/out/solidity/temp
 cd $PWDDIR/out/solidity/temp
-cmake $PWDDIR/test-suites/solidity -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ -DCMAKE_C_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang -DCMAKE_CXX_FLAGS="-flto=thin -save-stats=obj -fwhole-program-vtables -fuse-ld=lld -Wl,--lto-whole-program-visibility -Wl,--plugin-opt=-enable-dyncastopt=false" -DUSE_Z3=OFF
+cmake $PWDDIR/test-suites/solidity -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang++ -DCMAKE_C_COMPILER=$PWDDIR/toolchain/llvm-project/build-release/bin/clang -DCMAKE_CXX_FLAGS="-fvisibility=hidden -flto=thin -save-stats=obj -fwhole-program-vtables -fuse-ld=lld -Wl,--lto-whole-program-visibility -Wl,--plugin-opt=-enable-dyncastopt=false" -DUSE_Z3=OFF
 ninja solc
 mv $PWDDIR/out/solidity/temp/solc/solc $PWDDIR/out/solidity/thinlto
 mv $PWDDIR/out/solidity/temp/solc/main.cpp.stats $PWDDIR/out/solidity/thinlto/solc.stats
@@ -58,4 +58,4 @@ cmake $PWDDIR/test-suites/solidity -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -G Ni
 ninja solc
 mv $PWDDIR/out/solidity/temp/solc/solc $PWDDIR/out/solidity/fulllto
 mv $PWDDIR/out/solidity/temp/solc/main.cpp.stats $PWDDIR/out/solidity/fulllto/solc.stats
-
+##
