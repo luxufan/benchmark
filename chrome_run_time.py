416cb32f8bcf97b747445047f1037e3987dc53a8
luxufan <luxufan981014@gmail.com>
2024-12-29 04:57:19 +0800

    add chrome_run_time.py

---
+++ chrome_run_time.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    args = parser.parse_args()
+
+    file_path = args.file
+    f = open(args.file)
+    data = json.load(f)
+    tests = data['tests']['blink_perf.css']
+    for t in tests:
+        test = tests[t]
+        artifacts = test['artifacts']
+        measurements = artifacts['measurements.json']
+        data_file = measurements[0]
+        m_file = file_path[0:-len('test-results.json')] + data_file
+        m_f = open(m_file)
+        m_json = json.load(m_f)
+        measurements = m_json['measurements']
+        count = 0
+        unit = ''
+        for tt in measurements:
+            measurement = measurements[tt]
+            unit = measurement['unit']
+            sample = measurement['samples']
+            count = sum(sample) / len(sample)
+            print(count)
